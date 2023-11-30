4a2304a88314e08b5929a0c0c12f1b98c279824b
Ziraddin <zireddin.ismileoff@gmail.com>
2023-11-30 14:11:30 +0400

    Added :  SignOut functionality -> AccountFragment.kt ,  Navigation Animations -> nav_activity_main.xml,  Force light mode
    Solved :
    Navigation issues solved

--- app/src/main/java/com/example/flightapp/ui/fragments/transaction/TransactionFragment.kt
+++ app/src/main/java/com/example/flightapp/ui/fragments/transaction/TransactionFragment.kt
@@ -5,23 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.findNavController
 import com.example.flightapp.R
 import com.example.flightapp.databinding.FragmentTransactionBinding
 
 class TransactionFragment : Fragment() {
-   private lateinit var binding: FragmentTransactionBinding
+    private lateinit var binding: FragmentTransactionBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTransactionBinding.inflate(inflater)
         setNavigation()
         return binding.root
     }
 
-    private fun setNavigation(){
+    private fun setNavigation() {
+        binding.arrowBack.setOnClickListener {
+            findNavController().popBackStack(R.id.homeFragment, false)
+        }
+
         binding.btnTransaction.setOnClickListener {
             findNavController().navigate(R.id.action_transactionFragment_to_homeFragment)
         }
