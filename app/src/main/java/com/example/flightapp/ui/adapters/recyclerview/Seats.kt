a5eac778f3d844d4931180b0dae52ad20197514b
Nicat Zeynalli <nicat21308@gmail.com>
2023-12-01 11:42:38 +0400

    Added: seats

---
+++ app/src/main/java/com/example/flightapp/ui/adapters/recyclerview/Seats.kt
@@ -0,0 +1,7 @@
+package com.example.flightapp.ui.adapters.recyclerview
+
+data class Seats(
+    val imgSrc : Int ,
+    var isSelected: Boolean = false ,
+    var notAvailable :Boolean = false
+)
--- app/src/main/java/com/example/flightapp/ui/fragments/SelectSeatFragment.kt
+++ app/src/main/java/com/example/flightapp/ui/fragments/SelectSeatFragment.kt
@@ -6,26 +6,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.flightapp.R
 import com.example.flightapp.databinding.FragmentSelectSeatBinding
+import com.example.flightapp.ui.adapters.recyclerview.SeatAdapters
+import com.example.flightapp.ui.adapters.recyclerview.Seats
 
 class SelectSeatFragment : Fragment() {
-   private lateinit var binding : FragmentSelectSeatBinding
+    private lateinit var binding: FragmentSelectSeatBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= FragmentSelectSeatBinding.inflate(inflater)
+        binding = FragmentSelectSeatBinding.inflate(inflater)
         setNavigation()
+        setAdapter()
         return binding.root
     }
 
 
-    private fun setNavigation(){
+    private fun setNavigation() {
         binding.arrowBack.setOnClickListener {
             findNavController().popBackStack()
         }
     }
+
+    private fun setAdapter() {
+        val listOfSeats = listOf<Seats>(
+            Seats(R.drawable.seat_available),
+            Seats(R.drawable.seat_available),
+            Seats(R.drawable.seat_available),
+            Seats(R.drawable.seat_available),
+            Seats(R.drawable.seat_available,isSelected = true),
+            Seats(R.drawable.seat_available),
+            Seats(R.drawable.seat_available, notAvailable = true),
+            Seats(R.drawable.seat_available)
+        )
+        val adapter = SeatAdapters(listOfSeats)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
+
+    }
+
 
 }
